92bab31021b90cf76a47931f30f0332249d14662
Moamen Mostafa <68300756+moamen270@users.noreply.github.com>
2021-08-23 05:11:05 +0200

    Update Program.cs

--- Prime Numbers Checker/Program.cs
+++ Prime Numbers Checker/Program.cs
@@ -7,45 +7,46 @@ namespace Prime_Numbers_Checker
         static public class Prime
         {
            static public  bool  check(int PrimeNum)
-            {
+           {
                 // Check From 1 to -  
                 if (PrimeNum <= 1)
-                    return false;
-
-                else
                 {
-                    if (PrimeNum == 2)
-                        return true;
-                    // Check Even Number 
-                    if (PrimeNum % 2 == 0 || PrimeNum % 5 == 0)
-                       return false;
-                    else
-                    {
-                        if ( PrimeNum == 3 || PrimeNum == 5 ) 
-                            return true;
+                    Console.WriteLine("Steps Counter : 1 ");
+                    return false;
+                }
+
+                if (PrimeNum == 2 || PrimeNum == 3 || PrimeNum == 5)
+                {
+                    Console.WriteLine("Steps Counter : 2 ");
+                    return true;
+                }
+                // Check Even Number 
+                if (PrimeNum % 2 == 0 || PrimeNum % 5 == 0)
+                {
+                    Console.WriteLine("Steps Counter : 3 ");
+                    return false;
+                }
                         // Check Odd Numbers
                         //   *** n/i ***
                         // PrimeNum / 3 = R   -so-  PrimeNum / R = 3 
                         // if you checked a number you already checked the other side
-                        int counter = 0;
-                        for (int i = 2; i < PrimeNum / i; i += 2)
-                        {
-                            counter++;
-                            if (PrimeNum % (i + 1) == 0)
-                            {
-                                Console.WriteLine("Loop Counter : " + counter);
-                                return false;
-                            }
-                            // ignore Multiples   : Not completed yet 
-                            if ((i+1) % 5 == 0)
-                                i += 2;
-                            
-                        }
-                        Console.WriteLine("Loop Counter : " + counter);
-                        return true;
+                int counter = 3;
+                for (int i = 2; i < PrimeNum / i; i += 2)
+                {
+                    counter++;
+                    if (PrimeNum % (i + 1) == 0)
+                    {
+                        Console.WriteLine("Steps Counter : " + counter);
+                        return false;
                     }
+                    // ignore Multiples   : Not completed yet 
+                    if ((i + 3) % 5 == 0)
+                        i += 2;
                 }
-            }
+                Console.WriteLine("Steps Counter : " + counter);
+                return true;
+           }
+        }
             static void Main(string[] args)
             { 
                 try
@@ -76,4 +77,4 @@ namespace Prime_Numbers_Checker
         }
     }
 
-}
+
